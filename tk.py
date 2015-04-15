1564a4f72b68b19092c0943eb2e859fad93c9e9c
y1275963 <1275963@gmail.com>
2015-04-15 23:44:15 +0100

    Functioning single scraper:
    Specify main folder:
        1. create subfolder by 'date'
        2. create filename by 'title'

--- tk.py
+++ tk.py
@@ -7,6 +7,7 @@ from lxml.html.clean  import Cleaner
 import lxml.html as html
 import re 
 import os
+import datetime
 
 import requests
 
@@ -16,10 +17,19 @@ import urllib2
 class scraper:
     
     def __init__(self,link):
+        destionation = '/tmp'
+
         self.ori_link = link
         self.analyzer()
         
+        #define the download path:
+        self.loc = os.path.join(destionation,self.date)
+        if not os.path.exists(self.loc):
+            os.mkdir(self.loc)
+        
+        
         self.crawing()
+        self.txtsave()
         self.mp3_download()
         
     def analyzer(self):
@@ -29,18 +39,18 @@ class scraper:
         print_patter = 'http://learningenglish.voanews.com/articleprintview/'  
         self.print_link = print_patter + print_code
         
+        
         # get mp3 url:        
         response = requests.get(self.ori_link)
         
         if response.status_code != 200:
             print "Printing: Not 200"
-        html = response.content  
-            
-        
-        soup = BeautifulSoup(html)
+        html = response.content                  
+        soup = BeautifulSoup(html)      
         
         mp3list = soup.find('li',{'class':'downloadlink'})
         mp3list = mp3list.findAll('a',)
+      
         # mp3list:
         # 0: MP3 - 128.0kb/s ~8.3MB
         # 1: wav - 1.4Mb/s ~ 91.3MB
@@ -49,17 +59,25 @@ class scraper:
         mp3_link = mp3list[0]
         self.mp3_link = mp3_link['href']
         
+        # We can continue to use the soup:     
+        # get the title:        
+        self.title = soup.title.contents[0].strip().encode('utf8')
         
-        print self.mp3_link
+        # get the date:
+        date_temp = soup.find('p',{'class':'article_date'})
+        date = date_temp.contents[0].strip()
+        ##change the format:
+        self.date = datetime.datetime.strptime(date,'%m/%d/%Y').strftime('%y-%m-%d')
         
-            
-    
-    def txtsave(self,name_of_the_file):
-        text_file = open(name_of_the_file,'w')
+                  
+    def txtsave(self):
+        text_file = open(os.path.join(self.loc,self.title)+'.txt','w')
         text_file.write(self.article)
         text_file.close()
         
     def crawing(self):
+        # This is to get the content of the audio
+    
         response = requests.get(self.print_link)
         if response.status_code != 200:
             print "Printing: Not 200"
@@ -69,7 +87,6 @@ class scraper:
         Cleaner(kill_tags=['noscript'], style=True)(plain_html)
         
         article = plain_html.text_content()
-#        self.test = plain_html.text_content()
         
         remove_print = r'Print options   Print:     Include Comments (.*)    Include images'
         article = re.sub(remove_print,"",article,count=1)
@@ -82,7 +99,7 @@ class scraper:
         
     def mp3_download(self):
         mp3 = urllib2.urlopen(self.mp3_link)
-        open('tk.mp3','wb').write(mp3.read())
+        open(os.path.join(self.loc,self.title)+'.mp3','wb').write(mp3.read())
         
 
 
