d8f8aac716e0d86152ba712b9dd9a0c5847042a5
y1275963 <1275963@gmail.com>
2015-04-15 21:20:14 +0100

    Initial Commit

---
+++ tk.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 19:48:11 2015
+"""
+
+from selenium import webdriver
+from lxml.html.clean  import Cleaner
+import lxml.html as html
+import re 
+
+import urllib2
+
+class scraper:
+    
+    def __init__(self,link):
+        self.link = link
+        self.crawing()
+        self.html2string()
+            
+    def crawing(self):
+        response = urllib2.urlopen(self.link)
+        self.plain_html = response.read()
+#        wd = webdriver.Firefox()
+#        wd.get(self.link)   
+#        self.plain_html = wd.page_source
+#        
+#        wd.close()
+    
+    def string2txt(self,name_of_the_file):
+        text_file = open(name_of_the_file,'w')
+        text_file.write(self.article)
+        text_file.close()
+        
+    def html2string(self):
+        plain_html = html.document_fromstring(self.plain_html)
+        Cleaner(kill_tags=['noscript'], style=True)(plain_html)
+
+        article = plain_html.text_content()
+        remove_print = r'Print options   Print:     Include Comments (.*)    Include images'
+        article = re.sub(remove_print,"",article,count=1)
+        remove_nextline = "\n                 \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\n"
+        article = article.replace(remove_nextline,"",1)
+        
+        article = article.encode('utf8')
+        self.article = article
+
+        
+
+if __name__ == "__main__":
+    
+    test = scraper("http://learningenglish.voanews.com/articleprintview/2717607.html")
+    
+    test.string2txt('tk.txt')
+
+
+
