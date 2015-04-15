d26f00230221882e7d35fdd753ba7f194c734a53
y1275963 <1275963@gmail.com>
2015-04-15 21:49:31 +0100

    Improved regular expression filter to remove white space

--- tk.py
+++ tk.py
@@ -6,6 +6,7 @@ Created on Wed Apr 15 19:48:11 2015
 from lxml.html.clean  import Cleaner
 import lxml.html as html
 import re 
+import os
 
 import urllib2
 
@@ -33,22 +34,31 @@ class scraper:
     def html2string(self):
         plain_html = html.document_fromstring(self.plain_html)
         Cleaner(kill_tags=['noscript'], style=True)(plain_html)
-
+        
         article = plain_html.text_content()
+#        self.test = plain_html.text_content()
+        
         remove_print = r'Print options   Print:     Include Comments (.*)    Include images'
         article = re.sub(remove_print,"",article,count=1)
-        remove_nextline = "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\t\r"
-        article = article.replace(remove_nextline,"",1)
+        # There is a large open space in the opening paragraph        
+        remove_nextline = "(\r\n){10,}" 
+        article = re.sub(remove_nextline,"",article,count=1)
         
         article = article.encode('utf8')
         self.article = article
+        
+    def mp3_download(self):
+        test = 'file:///Users/y1275963/Downloads/8ff36304-8862-4091-84d9-be0b9bca5281_hq.mp3'
+        mp3 = urllib2.urlopen(test)
+        open('tk.mp3','wb').write(mp3.read())
+        
 
 
         
 
 if __name__ == "__main__":
     
-    test = scraper("http://learningenglish.voanews.com/articleprintview/2717607.html")
+    test = scraper("http://learningenglish.voanews.com/articleprintview/2711784.html")
     test.txtsave('tk.txt')
 
 
